37f722c61402cdd929e41dd13a60ccb5eb9ba672
Soliunas <simas.soliunas@gmail.com>
2023-06-28 09:35:48 +0300

    NEW

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -3,7 +3,6 @@ import { file } from './file.js';
 
 type Server = {
     init: () => void;
-    // httpServer: typeof http.createServer;
     httpServer: any;
 }
 
@@ -24,89 +23,134 @@ server.httpServer = http.createServer(async (req: IncomingMessage, res: ServerRe
     const textFileExtensions = ['css', 'js', 'svg', 'webmanifest'];
     const binaryFileExtensions = ['png', 'jpg', 'jpeg', 'webp', 'ico', 'eot', 'ttf', 'woff', 'woff2', 'otf'];
     const fileExtension = trimmedPath.slice(trimmedPath.lastIndexOf('.') + 1);
-
+    
     const isTextFile = textFileExtensions.includes(fileExtension);
     const isBinaryFile = binaryFileExtensions.includes(fileExtension);
-    const isAPI = trimmedPath.startsWith('api/');
+    const isAPI = trimmedPath.startsWith('api/');           
     const isPage = !isTextFile && !isBinaryFile && !isAPI;
 
-    // type Mimes = { [key: string]: string };
     type Mimes = Record<string, string>;
 
-    const MIMES: Mimes = {
-        html: 'text/html',
-        css: 'text/css',
-        js: 'text/javascript',
-        json: 'application/json',
-        txt: 'text/plain',
-        svg: 'image/svg+xml',
-        xml: 'application/xml',
-        ico: 'image/vnd.microsoft.icon',
-        jpeg: 'image/jpeg',
-        jpg: 'image/jpeg',
-        png: 'image/png',
-        webp: 'image/webp',
-        woff2: 'font/woff2',
-        woff: 'font/woff',
-        ttf: 'font/ttf',
-        webmanifest: 'application/manifest+json',
-    };
+        const MIMES: Mimes = {
+            html: 'text/html',
+            js: 'text/javascript',
+            json: 'application/json',
+            txt: 'text/plain',
+            webmanifest: 'application/manifest+json',
+        };
 
-    let responseContent: string | Buffer = 'ERROR: neturiu tai ko tu nori...';
-
+    let responseContent = 'ERROR: neturiu tai ko tu nori...';;
+    
     if (isTextFile) {
-        const [err, msg] = await file.readPublic(trimmedPath);
-        res.writeHead(err ? 404 : 200, {
-            'Content-Type': MIMES[fileExtension],
-            'cache-control': `max-age=60`,
-        });
-        if (err) {
-            responseContent = msg;
-        } else {
-            responseContent = msg;
-        }
+        responseContent = 'TEKSTINIS FAILAS';
     }
 
     if (isBinaryFile) {
-        const [err, msg] = await file.readPublicBinary(trimmedPath);
-        res.writeHead(err ? 404 : 200, {
-            'Content-Type': MIMES[fileExtension],
-            'cache-control': `max-age=60`,
-        });
+        responseContent = 'BINARY FAILAS';
+    }
+
+    if (isAPI) {
+        const content = `{
+            "id": 22,
+            "name": "Jonas",
+            "email": "jonas@juozapaitis.lt",
+            "password": 4321
+        }`;
+        const [err, msg] = await file.create('../data', 'jonas@juozapaitis.lt.json', content);
         if (err) {
             responseContent = msg;
         } else {
-            responseContent = msg;
+            responseContent = content;
         }
     }
 
     if (isAPI) {
-        responseContent = 'API DUOMENYS';
+        const content = `{
+            "id": 11,
+            "name": "Maryte",
+            "email": "maryte@mariauskiate.lt",
+            "password": 1122
+        }`;
+        const [err, msg] = await file.create('../data', 'maryte@mariauskiate.lt.json', content);
+        if (err) {
+            responseContent = msg;
+        } else {
+            responseContent = content;
+        }
+    }
+
+    if (isAPI) {
+        const content = `{
+            "id": 33,
+            "name": "Petras",
+            "email": "petras@petraitis.lt",
+            "password": 1234
+        }`;
+        const [err, msg] = await file.create('../data', 'petras@petraitis.lt.json', content);
+        if (err) {
+            responseContent = msg;
+        } else {
+            responseContent = content;
+        }
+    }
+
+    if (isAPI) {
+        const content = `{
+            "id": 11,
+            "name": "Maryte",
+            "email": "maryte@mariauskaite.lt",
+            "password": 1122
+        }`;
+        const [err, msg] = await file.delete('../data', 'maryte@mariauskaite.lt.json');
+        if (err) {
+            responseContent = msg;
+        } else {
+            responseContent = content;
+        }
+    }
+
+    if (isAPI) {
+
+        const content = `{
+            "id": 22,
+            "name": "Jonas",
+            "email": "jonas@juozapaitis.lt",
+            "password": 4321
+        }`;
+
+        const [err, msg] = await file.update('../data', 'jonas@juozapaitis.lt.json', content);
+        if (err) {
+            responseContent = msg;
+        } else {
+            responseContent = content;
+        }
+    }
+
+    if (isAPI) {
+        const content = `{
+            "id": 33,
+            "name": "Petras",
+            "email": "petras@petraitis.lt",
+            "password": 1234
+        }`;
+        const [err, msg] = await file.update('../data', 'petras@petraitis.lt.json', content);
+        if (err) {
+            responseContent = msg;
+        } else {
+            responseContent = content;
+        }
     }
 
     if (isPage) {
-        let fileResponse = await file.read('../pages', trimmedPath + '.html');
-        let [err, msg] = fileResponse;
-
-        if (err) {
-            fileResponse = await file.read('../pages', '404.html');
-            err = fileResponse[0];
-            msg = fileResponse[1];
-        }
-
-        res.writeHead(err ? 404 : 200, {
-            'Content-Type': MIMES.html,
-        });
-
-        responseContent = msg as string;
+        responseContent = `LABAI GERA SVETAINĖ`;
     }
 
     return res.end(responseContent);
 });
 
 server.init = () => {
-    server.httpServer.listen(4410, () => {
-        console.log('Serveris sukasi ant http://localhost:4410');
+    server.httpServer.listen(3016, () => {
+        console.log('Serveris sukasi ant http://localhost:3016');
     });
 };
 
